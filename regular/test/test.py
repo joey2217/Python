56501d539d4c9aed5b6e69c9526c4678cfc4259c
BurNing1993 <786678257@qq.com>
2018-04-16 22:00:07 +0800

    reg

---
+++ regular/test/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+line = "XXX出生于2001年6月1日"
+line = "XXX出生于2001/6/1"
+line = "XXX出生于2001-6-1"
+line = "XXX出生于2001-06-01"
+line = "XXX出生于2001-06"
+
+regex_str = ".*出生于(\d{4}[年/-]\d{1,2}([月/-]\d{1,2}|[日/-]$|$))"
+match_obj = re.match(regex_str, line)
+if match_obj:
+    print(match_obj.group(1))
